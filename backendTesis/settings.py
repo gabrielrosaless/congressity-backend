b0da8154382871bdaa25de3923947ef467934aa3
gabrielrosaless <gabrielpprosales@gmail.com>
2021-11-08 22:52:12 -0300

    commit 2

--- backendTesis/settings.py
+++ backendTesis/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'backendTesis.urls'
